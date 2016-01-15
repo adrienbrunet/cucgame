e2c787325b928c895976bb1f875ae3ef7769c67f
Adrien <abrunet@centrale-marseille.fr>
2016-01-15 20:42:19 +0100

    Template base and basic view

--- cucgame/urls.py
+++ cucgame/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'cucgame.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('game.urls')),
+    url(r'^internet_explorer/', views.InternetExplorer.as_view(), name='internet_explorer')
 ]
---
+++ cucgame/views.py
@@ -0,0 +1,5 @@
+from django.views.generic import TemplateView
+
+
+class InternetExplorer(TemplateView):
+    template_name = "internet_explorer.html"
