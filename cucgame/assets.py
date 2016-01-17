55517567ecf8d2af4edee84ddba84da70eeb779f
Adrien <abrunet@centrale-marseille.fr>
2016-01-17 21:41:32 +0100

    static css files + assets update

---
+++ cucgame/assets.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+
+from django_assets import Bundle, register
+
+
+css_main = Bundle(
+    'bootstrap/dist/css/bootstrap.css',
+    'font-awesome/css/font-awesome.css',
+
+    filters=('cssrewrite', 'yui_css',),
+    output='compiled-assets/gen/css_main.%(version)s.css'
+)
+
+register('css_main', css_main)
+
+js_main = Bundle(
+    'lodash/lodash.js',
+
+    'angular/angular.js',
+
+    'angular-animate/angular-animate.js',
+    'angular-cookies/angular-cookies.js',
+    'angular-resource/angular-resource.js',
+    'angular-route/angular-route.js',
+
+    'js/angular-app/game/module.js',
+    'js/angular-app/game/services.js',
+    'js/angular-app/game/resources.js',
+    'js/angular-app/game/controllers.js',
+
+    'js/angular-app/dispatch.js',
+
+    filters='closure_js',
+    output='compiled-assets/gen/js_main.%(version)s.js')
+
+register('js_main', js_main)
